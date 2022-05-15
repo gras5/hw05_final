f1ecade67ebbd0a5c54e5fe4871cc879c2c7f43d
Rafis Gaisin <gajsrafis@yandex.ru>
2022-05-15 14:58:46 +0300

    Fixed 403 method

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -5,7 +5,7 @@ def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
-def csrf_failure(request, reason=''):
+def csrf_failure(request, exception):
     return render(request, 'core/403csrf.html', status=403)
 
 
